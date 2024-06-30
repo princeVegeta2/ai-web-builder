056548ab901c49dedfdc036d01dfb276a687fe23
princeVegeta2 <nellatorbeaufort@gmail.com>
2024-06-30 17:59:08 +0200

    Drag-drop implemented

--- src/components/pages/WebBuilder.js
+++ src/components/pages/WebBuilder.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react';
 import '../../assets/styles/WebBuilder.css';
 import plusSymbol from '../../assets/images/plus-symbol.png';
 import trashcan from '../../assets/images/trashcan.png';
-import DraggableComponent from '../common/DraggableComponent';
-import DroppableArea from '../common/DroppableArea';
 
 function WebBuilder() {
-  const [windows, setWindows] = useState([{ id: 1, components: [] }]);
+  const [windows, setWindows] = useState([{ id: 1, widgets: [] }]);
 
   const addWindow = (id) => {
-    const newWindow = { id: windows.length + 1, components: [] };
+    const newWindow = { id: windows.length + 1, widgets: [] };
     const updatedWindows = windows.map(window =>
       window.id === id ? { ...window, hasPlusButton: false } : window
     );
@@ -24,13 +22,23 @@ function WebBuilder() {
     setWindows(remainingWindows);
   };
 
-  const handleDrop = (windowId, component) => {
+  const handleOnDrag = (e, widgetType) => {
+    e.dataTransfer.setData("widgetType", widgetType);
+  };
+
+  const handleOnDrop = (e, windowId) => {
+    e.preventDefault();
+    const widgetType = e.dataTransfer.getData("widgetType");
     const updatedWindows = windows.map(window =>
       window.id === windowId
-        ? { ...window, components: [...window.components, component] }
+        ? { ...window, widgets: [...window.widgets, widgetType] }
         : window
     );
     setWindows(updatedWindows);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
   };
 
   const components = [
@@ -47,28 +55,41 @@ function WebBuilder() {
         <h2>Components</h2>
         <ul>
           {components.map((component, index) => (
-            <DraggableComponent key={index} name={component.name} />
+            <li
+              key={index}
+              draggable
+              onDragStart={(e) => handleOnDrag(e, component.name)}
+              className="draggable-component"
+            >
+              {component.name}
+            </li>
           ))}
         </ul>
       </div>
       <div className="webbuilder">
         {windows.map((window) => (
           <div key={window.id} className="workspace-window">
-            <DroppableArea onDrop={(component) => handleDrop(window.id, component)}>
-              <div className="rectangle">
-                {window.components.map((component, index) => (
-                  <div key={index} className="component">{component}</div>
+            <div className="rectangle-container">
+              <div
+                className="rectangle"
+                onDrop={(e) => handleOnDrop(e, window.id)}
+                onDragOver={handleDragOver}
+              >
+                {window.widgets.map((widget, index) => (
+                  <div key={index} className="component">
+                    {widget}
+                  </div>
                 ))}
-                {window.id !== 1 && (
-                  <img
-                    src={trashcan}
-                    alt="Remove Window"
-                    className="trashcan-button"
-                    onClick={() => removeWindow(window.id)}
-                  />
-                )}
               </div>
-            </DroppableArea>
+              {window.id !== 1 && (
+                <img
+                  src={trashcan}
+                  alt="Remove Window"
+                  className="trashcan-button"
+                  onClick={() => removeWindow(window.id)}
+                />
+              )}
+            </div>
             {window.hasPlusButton !== false && (
               <img
                 src={plusSymbol}
